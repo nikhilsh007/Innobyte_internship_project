0c5a0bd1e32cc36db652a187ad126094fc239940
nikhilsh007 <145344464+nikhilsh007@users.noreply.github.com>
2024-08-03 22:37:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// display the menu function
+function displaymenu(){
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");
+}
+
+// display page
+function displaypage(){
+    var menu = document.getElementById("menu");
+    menu.classList.add("hidden");
+}
+
